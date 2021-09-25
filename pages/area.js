09031eb7b09c31da6833a44ca4a8419ce004e920
Rohit Prasad <pdrohit674@gmail.com>
2021-09-25 12:43:56 +0530

    Fix: formula of calculating area

--- pages/area.js
+++ pages/area.js
@@ -2,7 +2,7 @@ var inputSide = document.querySelectorAll(".input-side");
 var calculateBtn = document.querySelector("#calculate");
 var output = document.querySelector("#output");
 
-function checkValidity(a, b, c){
+function checkValidity(a, b, c) {
     if (a + b <= c || a + c <= b || b + c <= a)
         return false;
     else
@@ -10,47 +10,40 @@ function checkValidity(a, b, c){
 }
 
 function calculateArea(side1, side2, side3) {
-    if(checkValidity(side1,side2,side3)){
+    if (checkValidity(side1, side2, side3)) {
         var p = (side1 + side2 + side3) / 2;
-        var res = p *(p-side1)*(p*side2)*(p-side3);
+        var res = p * (p - side1) * (p - side2) * (p - side3);
         var area = Math.sqrt(res);
         showMessage(area)
     }
-    else
-    {
+    else {
         showErrorMessage("invalid sides")
     }
 
 }
 
-function showMessage(area)
-{
+function showMessage(area) {
     output.innerText = "The area of the triangle is " + area.toFixed(3) + "cm²";
 }
 
-function showErrorMessage(msg)
-{
+function showErrorMessage(msg) {
     output.innerText = msg;
 }
 
 
-calculateBtn.addEventListener("click", function() {
-    if(inputSide[0].value && inputSide[1].value && inputSide[2].value)
-    {
-        if(inputSide[0].value > 0 && inputSide[1].value > 0 && inputSide[2].value > 0){
-            calculateArea(Number(inputSide[0].value), Number(inputSide[1].value),Number(inputSide[2].value));
+calculateBtn.addEventListener("click", function () {
+    if (inputSide[0].value && inputSide[1].value && inputSide[2].value) {
+        if (inputSide[0].value > 0 && inputSide[1].value > 0 && inputSide[2].value > 0) {
+            calculateArea(Number(inputSide[0].value), Number(inputSide[1].value), Number(inputSide[2].value));
         }
-        else if(inputSide[0].value < 0 || inputSide[1].value < 0 ||inputSide[2].value < 0)
-        {
+        else if (inputSide[0].value < 0 || inputSide[1].value < 0 || inputSide[2].value < 0) {
             showErrorMessage("Please enter postive values!");
         }
-        else
-        {
+        else {
             showErrorMessage("Please enter all the inputs, input cannot be zero")
         }
     }
-    else
-    {
+    else {
         showErrorMessage("Invalid Input!")
     }
 })
